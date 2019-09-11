c6809197854f86426ff5a84b5e54dbfae78a37cb
Casper <CasualX@users.noreply.github.com>
2019-09-11 15:34:49 +0200

    Assist injected DLL code execution (#26)

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -7,3 +7,12 @@ mod module_ldr_data;
 
 pub use self::module_enum::*;
 pub use self::module_ldr_data::*;
+
+extern "C" {
+	static __ImageBase: u8;
+}
+
+/// Returns this module's image base.
+pub fn image_base() -> crate::winapi::HMODULE {
+	unsafe { &__ImageBase as *const _ as crate::winapi::HMODULE }
+}
